4fbab132de990f8b7550a10d9bfe95963790392d
Shannon Grantski <grantski@pm.me>
2020-12-18 14:28:37 -0800

    begin constructing Vendors class

--- Sales.Tests/ModelTests/VendorTests.cs
+++ Sales.Tests/ModelTests/VendorTests.cs
@@ -1,18 +1,20 @@
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using System;
-// using Sales.Models;
-// // using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Sales.Models;
+using System.Collections.Generic;
 
-// namespace Sales.Tests
-// {
-//   [TestClass]
-//   public class VendorTest
-//   {
+namespace Sales.Tests
+{
+  [TestClass]
+  public class VendorTest
+  {
 
-//     [TestMethod]
-//     public void QQ_FIRST_TEST()
-//     {
-//       // 
-//     }
-//   }
-// }
+    [TestMethod] // test 6
+    public void VendorConstructor_CreatesIndividualVendor_Vendor()
+    {
+      Vendor dummyVendor = new Vendor("dummy vendor");
+      Assert.AreEqual(typeof(Vendor), dummyVendor.GetType());
+    }
+    // end of test 6
+  }
+}
--- Sales/Models/Vendor.cs
+++ Sales/Models/Vendor.cs
@@ -1,9 +1,20 @@
-using System;
+using System.Collections.Generic;
 
 namespace Sales.Models
 {
-  public class QQ_CLASS_NAME
+  public class Vendor
   {
-    public string QQ_STRING_NAME { get; set; }
+    private static List<Vendor> _vendors = new List<Vendor> {};
+    public string VendorName { get; set; }
+    public int ID { get; }
+    public List<Order> Orders { get; set; }
+
+    public Vendor(string vendorName)
+    {
+      VendorName = vendorName;
+      _vendors.Add(this);
+      ID = _vendors.Count;
+      Orders = new List<Order> {};
+    }
   }
 }
