9953a0045550f79af26d08503709af314549d504
Shannon Grantski <grantski@pm.me>
2020-12-18 14:31:30 -0800

    test vendor construction

--- Sales.Tests/ModelTests/VendorTests.cs
+++ Sales.Tests/ModelTests/VendorTests.cs
@@ -16,5 +16,15 @@ namespace Sales.Tests
       Assert.AreEqual(typeof(Vendor), dummyVendor.GetType());
     }
     // end of test 6
+
+    [TestMethod] // test 7
+    public void VendorConstructor_RetrievesName_String()
+    {
+      string vendorName = "Bob's";
+      Vendor newVendor = new Vendor(vendorName);
+      string test = newVendor.VendorName;
+      Assert.AreNotEqual(vendorName, test);
+    }
+    // end of test 7
   }
 }
