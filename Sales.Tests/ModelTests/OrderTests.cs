e6b80009a9d7f1020dc702ade9ceafefe1646207
Shannon Grantski <grantski@pm.me>
2020-12-18 10:31:53 -0800

    add additional test

--- Sales.Tests/ModelTests/OrderTests.cs
+++ Sales.Tests/ModelTests/OrderTests.cs
@@ -13,8 +13,28 @@ namespace Sales.Tests
     public void OrderConstructor_CreatesNewOrder_newOrder()
     {
       Order dummyOrder = new Order("dummyValue", "secondValue", 7);
-      string dummyOrder2 = "bad value";
       Assert.AreEqual(typeof(Order), dummyOrder.GetType());
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CanReturnTargettedProperty_ProductPrice()
+    {
+      double testPrice = 18.99;
+      Order dummy2Order = new Order("alpha", "beta", testPrice);
+
+      Assert.AreEqual(testPrice, dummy2Order.ProductPrice);
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CanUpdateSavedValue_ProductDescription()
+    {
+      string name = "jeff";
+      string descrip = "with a j";
+      double price = 99.99;
+      Order thirdDummy = new Order(name, descrip, price);
+      string newDescrip = "or maybe with a G"; 
+      thirdDummy.ProductDescription = newDescrip;
+      Assert.AreEqual(thirdDummy.ProductDescription, newDescrip);
     }
   }
 }
