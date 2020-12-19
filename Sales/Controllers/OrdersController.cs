01b9329b0f2bbbd9355277aa51c3c86da1c9e2db
Shannon Grantski <grantski@pm.me>
2020-12-18 17:01:36 -0800

    adjust Views and Controller

--- Sales/Controllers/OrdersController.cs
+++ Sales/Controllers/OrdersController.cs
@@ -6,30 +6,36 @@ namespace Sales.Controllers
 {
   public class OrdersController : Controller
   {
-    [HttpGet("/orders")]
-    public ActionResult Index()
+    // [HttpGet("/vendors/{vendorsid}/orders")]
+    // public ActionResult Index()
+    // {
+    //   List<Order> allOrders = Order.Collect();
+    //   return View(allOrders);
+    // }
+
+    [HttpGet("/vendors/{vendorsid}/orders/new")]
+    public ActionResult New(int vendorID)
     {
-      List<Order> allOrders = Order.Collect();
-      return View(allOrders);
+      Vendor targettedVendor = Vendor.Lookup(vendorID);
+      return View(targettedVendor);
     }
 
-    [HttpGet("/orders/new")]
-    public ActionResult New()
-    {
-      return View();
-    }
-
-    [HttpPost("/orders/delete")]
+    [HttpPost("/vendors/{vendorsid}/orders/delete")]
     public ActionResult DeleteAll()
     {
       Order.ClearAll();
       return View();
     }
 
-    [HttpGet("/orders/{id}")]
-    public ActionResult Show(int id)
-    {Order calledOrder = Order.Lookup(id);
-    return View(calledOrder);
+    [HttpGet("/vendors/{vendorsid}/orders/{ordersid}")]
+    public ActionResult Show(int vendorID, int orderID)
+    {
+      Order calledOrder = Order.Lookup(orderID);
+      Vendor targettedVendor = Vendor.Lookup(vendorID);
+      Dictionary<string, object> salesModel = new Dictionary<string, object>();
+      salesModel.Add("order", calledOrder);
+      salesModel.Add("vendor", targettedVendor);
+      return View(salesModel);
     }
   }
 }
