325793c9667916082f65d5a952c76382c3fa3b52
Shannon Grantski <grantski@pm.me>
2021-01-03 14:44:33 -0800

    add index for orders

--- Sales/Controllers/VendorsController.cs
+++ Sales/Controllers/VendorsController.cs
@@ -1,6 +1,6 @@
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
 using Sales.Models;
 
 namespace Sales.Controllers
@@ -13,17 +13,20 @@ namespace Sales.Controllers
       List<Vendor> allVendors = Vendor.Collect();
       return View(allVendors);
     }
+    
     [HttpGet("/vendors/new")]
     public ActionResult New()
     {
       return View();
     }
+
     [HttpPost("/vendors")]
     public ActionResult Create(string vendorName)
     {
       Vendor aNewVendor = new Vendor(vendorName);
       return RedirectToAction("Index");
     }
+
     [HttpGet("/vendors/{id}")]
     public ActionResult Show(int id)
     {
@@ -33,6 +36,20 @@ namespace Sales.Controllers
       salesModel.Add("vendor", chosenVendor);
       salesModel.Add("orders", vendorsOrders);
       return View(salesModel);
+    }
+
+    ////
+    [HttpPost("/vendors/{vendorID}/orders")]
+    public ActionResult Create(int vendorID, string productName, string productDescription, double productPrice)
+    {
+      Dictionary<string, object> salesModel = new Dictionary<string, object>();
+      Vendor targettedVendor = Vendor.Lookup(vendorID);
+      Order newOrder = new Order(productName, productDescription, productPrice); 
+      targettedVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = targettedVendor.Orders;
+      salesModel.Add("order", vendorOrders);
+      salesModel.Add("vendor", targettedVendor);
+      return View("Show", salesModel);
     }
   }
 }
