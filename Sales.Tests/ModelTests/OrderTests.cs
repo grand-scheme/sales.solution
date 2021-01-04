aca2aeea954cef189db42eb05ac77ae098e7f493
Shannon Grantski <grantski@pm.me>
2021-01-04 05:54:15 -0800

    update all double/string discrepancies

--- Sales.Tests/ModelTests/OrderTests.cs
+++ Sales.Tests/ModelTests/OrderTests.cs
@@ -31,40 +31,40 @@ namespace Sales.Tests
     }
     // end of test 2
 
-    // [TestMethod] // test 3
-    // public void OrderConstructor_CanUpdateSavedValue_Description()
-    // {
-    //   string name = "jeff";
-    //   string descrip = "with a j";
-    //   double price = 99.99;
-    //   Order thirdDummy = new Order(name, descrip, price);
-    //   string newDescrip = "or maybe with a G"; 
-    //   thirdDummy.Description = newDescrip;
-    //   Assert.AreEqual(thirdDummy.Description, newDescrip);
-    // }
-    // // end of test 3
+    [TestMethod] // test 3
+    public void OrderConstructor_CanUpdateSavedValue_Description()
+    {
+      string name = "jeff";
+      string descrip = "with a j";
+      string price = "99.99";
+      Order thirdDummy = new Order(name, descrip, price);
+      string newDescrip = "or maybe with a G"; 
+      thirdDummy.Description = newDescrip;
+      Assert.AreEqual(thirdDummy.Description, newDescrip);
+    }
+    // end of test 3
 
-    // [TestMethod] // test 4
-    // public void OrderConstructor_SetAnId_Id()
-    // {
-    //   string name = "geoff";
-    //   string descrip = "with a g";
-    //   double price = 199.99;
-    //   Order fourthDummy = new Order(name, descrip, price);
-    //   Assert.AreEqual(1, fourthDummy.Id);
-    // }
-    // // end of test 4
+    [TestMethod] // test 4
+    public void OrderConstructor_SetAnId_Id()
+    {
+      string name = "geoff";
+      string descrip = "with a g";
+      string price = "199.99";
+      Order fourthDummy = new Order(name, descrip, price);
+      Assert.AreEqual(1, fourthDummy.Id);
+    }
+    // end of test 4
 
-    // [TestMethod] // test 5
-    // public void OrderConstructor_RetrieveById_OrderId()
-    // {
-    //   Order test1 = new Order("name1", "description1", 17.50);
-    //   Order test2 = new Order("name2", "description2", 27.50);
-    //   Order test3 = new Order("name3", "description3", 37.50);
-    //   //
-    //   Order jimbo = Order.Find(3);
-    //   Assert.AreEqual(jimbo.Id, test3.Id);
-    // }
+    [TestMethod] // test 5
+    public void OrderConstructor_RetrieveById_OrderId()
+    {
+      Order test1 = new Order("name1", "description1", "17.50");
+      Order test2 = new Order("name2", "description2", "27.50");
+      Order test3 = new Order("name3", "description3", "37.50");
+      //
+      Order jimbo = Order.Find(3);
+      Assert.AreEqual(jimbo.Id, test3.Id);
+    }
     // end of test 5
   }
 }
